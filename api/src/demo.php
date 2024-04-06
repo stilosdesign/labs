a2a26b8014c2852d2da4bcd62ef035044a912c7a
antonielbordin <antonielbordin@hotmail.com>
2024-04-06 12:04:28 -0300

    changing project data in composer.json and adding demo class

---
+++ api/src/demo.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+namespace ApiSwoole;
+
+/**
+ * <b>Demo</b>
+ * 
+ * Demos is the Class example
+ *
+ * @author Antoniel Bordin <webmaster@stilosdesign.com.br>
+ * @copyright © 2024, Stilos Design
+ * @version 1.0.0
+ * @since release 1.0.0
+ */
+class Demo
+{
+  // TODO: Evaluate possible implementation
+}
